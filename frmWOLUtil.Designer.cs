86699afdbe58367cd983b68dc679b14d2d4ca952
meowdin <meowdin@yahoo.com.sg>
2015-11-02 15:34:00 +0800

    Initial Commit
    C# Express 2010 Project  Directory
    Send WOL Packet to predefined list of pc stored in txt

---
+++ frmWOLUtil.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace WOL
+{
+    partial class frmWOLUtil
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.MAC = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Enable = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel_RecordFound = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.ToolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.btnEdit = new System.Windows.Forms.ToolStripButton();
+            this.btnReload = new System.Windows.Forms.ToolStripButton();
+            this.btnSend = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.ToolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeight = 20;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.MAC,
+            this.Enable});
+            this.dataGridView1.Location = new System.Drawing.Point(0, 69);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(641, 282);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // MAC
+            // 
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.DarkGray;
+            this.MAC.DefaultCellStyle = dataGridViewCellStyle1;
+            this.MAC.HeaderText = "Mac Address";
+            this.MAC.Name = "MAC";
+            // 
+            // Enable
+            // 
+            this.Enable.FillWeight = 20F;
+            this.Enable.HeaderText = "Select";
+            this.Enable.Name = "Enable";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel_RecordFound,
+            this.toolStripProgressBar1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 354);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(641, 25);
+            this.statusStrip1.TabIndex = 27;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel_RecordFound
+            // 
+            this.toolStripStatusLabel_RecordFound.AutoSize = false;
+            this.toolStripStatusLabel_RecordFound.BorderSides = System.Windows.Forms.ToolStripStatusLabelBorderSides.Right;
+            this.toolStripStatusLabel_RecordFound.Name = "toolStripStatusLabel_RecordFound";
+            this.toolStripStatusLabel_RecordFound.Size = new System.Drawing.Size(160, 20);
+            this.toolStripStatusLabel_RecordFound.Text = "Records : 0";
+            this.toolStripStatusLabel_RecordFound.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 19);
+            this.toolStripProgressBar1.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.toolStripProgressBar1.ToolTipText = "Loading...";
+            this.toolStripProgressBar1.Visible = false;
+            // 
+            // ToolStrip1
+            // 
+            this.ToolStrip1.AutoSize = false;
+            this.ToolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripSeparator1,
+            this.btnEdit,
+            this.btnReload});
+            this.ToolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.ToolStrip1.Name = "ToolStrip1";
+            this.ToolStrip1.Size = new System.Drawing.Size(641, 38);
+            this.ToolStrip1.TabIndex = 28;
+            this.ToolStrip1.Text = "ToolStrip1";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 38);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Image = global::WOL.Properties.Resources.TB_Edit;
+            this.btnEdit.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnEdit.Margin = new System.Windows.Forms.Padding(10, 1, 0, 2);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(48, 35);
+            this.btnEdit.Text = "Edit List";
+            this.btnEdit.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnEdit.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnReload
+            // 
+            this.btnReload.Image = global::WOL.Properties.Resources.TB_Refresh;
+            this.btnReload.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnReload.Margin = new System.Windows.Forms.Padding(10, 1, 0, 2);
+            this.btnReload.Name = "btnReload";
+            this.btnReload.Size = new System.Drawing.Size(62, 35);
+            this.btnReload.Text = "Reload List";
+            this.btnReload.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnReload.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnReload.Click += new System.EventHandler(this.btnReload_Click);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(0, 40);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(141, 23);
+            this.btnSend.TabIndex = 29;
+            this.btnSend.Text = "Turned On Selected";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // frmWOLUtil
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(641, 379);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.ToolStrip1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "frmWOLUtil";
+            this.Text = "Wake On Lan Utility";
+            this.Load += new System.EventHandler(this.frmWOLUtil_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ToolStrip1.ResumeLayout(false);
+            this.ToolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel_RecordFound;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        internal System.Windows.Forms.ToolStrip ToolStrip1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripButton btnReload;
+        private System.Windows.Forms.ToolStripButton btnEdit;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MAC;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Enable;
+    }
+}
+
---
+++ frmWOLUtil.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.Net.Sockets;
+using System.Net;
+using System.Globalization;
+using System.IO;
+/*
+ * Wake On Lan Utility
+ * List out all mac address stotred in txt file "public_file.txt"
+ * Allow user to select (check) and send WOL packet to the mac address specified in the txt file.
+ * For specific part sending the packet, refer: 
+ * 
+ * private void WakeFunction(string MAC_ADDRESS)
+ 
+ 
+ */
+namespace WOL
+{
+    public partial class frmWOLUtil : Form
+    {
+        CheckBox checkboxHeader = new CheckBox();
+        
+        public List<string> maclist = new List<string>();
+
+        //Config file location
+        public string appath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\WFUtil\";
+        
+        public frmWOLUtil()
+        {
+            InitializeComponent();
+          
+            if (!Directory.Exists(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)))
+            {
+
+                Directory.CreateDirectory(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData));
+            }
+            if (!Directory.Exists(appath))
+            {
+                Directory.CreateDirectory(appath);
+            }
+       
+        }
+        private void frmWOLUtil_Load(object sender, EventArgs e)
+        {
+            loadList();
+            show_chkBox();
+            checkboxHeader.Checked = true;
+        }
+        private void show_chkBox()
+        {
+            Rectangle rect = dataGridView1.GetCellDisplayRectangle(1, -1, true);
+            // set checkbox header to center of header cell. +1 pixel to position 
+            rect.Y = 2;
+            rect.X = rect.Location.X +(rect.Width-36);
+            checkboxHeader.Name = "checkboxHeader";
+            checkboxHeader.Size = new Size(18, 18);
+            checkboxHeader.Location = rect.Location;
+            checkboxHeader.CheckedChanged += new EventHandler(checkboxHeader_CheckedChanged);
+            dataGridView1.Controls.Add(checkboxHeader);
+        }
+        private void checkboxHeader_CheckedChanged(object sender, EventArgs e)
+        {
+
+            CheckBox headerBox = ((CheckBox)dataGridView1.Controls.Find("checkboxHeader", true)[0]);
+            int index = 0;
+            for (int i = 0; i < dataGridView1.RowCount; i++)
+            {
+                dataGridView1.Rows[i].Cells[1].Value = headerBox.Checked;
+            }
+        }
+     
+        private void writefile()
+        {
+
+            try
+            {
+                StreamWriter s = new StreamWriter(appath + "public_file.txt");
+                s.WriteLine("Write the MAC Addresses below:");
+
+
+                foreach (string line in maclist)
+                {
+
+                    s.WriteLine(line);
+
+                }
+
+
+                s.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+
+            }
+
+
+        }
+        private void loadList()
+        {
+
+
+            try
+            {
+
+                if (!File.Exists(appath + "public_file.txt"))
+                {
+
+                    writefile();
+
+                }
+
+                StreamReader file = new StreamReader(appath + "public_file.txt");
+                string line = "";
+                while ((line = file.ReadLine()) != null)
+                {
+                    if (!line.Trim().Contains(" "))
+                    {
+                        maclist.Add(line);
+
+                    }
+                }
+
+                file.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+
+            }
+            this.dataGridView1.Rows.Clear();
+
+
+            foreach (string line in maclist)
+            {
+
+                this.dataGridView1.Rows.Add(line, true);
+
+            }
+
+
+        }
+
+
+        private void WakeFunction(string MAC_ADDRESS)
+        {
+            WOLClass client = new WOLClass();
+            client.Connect(new
+               IPAddress(0xffffffff),  //255.255.255.255  i.e broadcast
+               0x2fff); // port=12287 default WOL port
+ 
+            client.SetClientToBrodcastMode();
+            
+            //set sending bites
+            int counter = 0;
+            
+            //buffer to be send
+            byte[] bytes = new byte[1024];   // more than enough :-)
+            
+            //first 6 bytes should be 0xFF
+            for (int y = 0; y < 6; y++)
+                bytes[counter++] = 0xFF;
+            
+            //now repeate sneding MAC 16 times
+            for (int y = 0; y < 16; y++)
+            {
+                int i = 0;
+                for (int z = 0; z < 6; z++)
+                {
+                    bytes[counter++] =
+                        byte.Parse(MAC_ADDRESS.Substring(i, 2),
+                        NumberStyles.HexNumber);
+                    i += 2;
+                }
+            }
+
+            //now send wake up packet
+            int reterned_value = client.Send(bytes, 1024);
+        }
+
+        private void btnReload_Click(object sender, System.EventArgs e)
+        {
+
+            loadList();
+        }
+
+        private void btnEdit_Click(object sender, System.EventArgs e)
+        {
+            if (!File.Exists(appath + "public_file.txt"))
+            {
+
+
+                writefile();
+
+            }
+            System.Diagnostics.Process.Start(appath + "public_file.txt");
+          
+        }
+
+     
+
+        private void btnSend_Click(object sender, System.EventArgs e)
+        {
+            checkboxHeader.Checked = true;
+
+
+            for (int i = 0; i < dataGridView1.RowCount; i++)
+            {
+                if ((bool)dataGridView1.Rows[i].Cells[1].Value)
+                {
+
+                    Thread t1 = new Thread(turnon);
+                    t1.Start(dataGridView1.Rows[i].Cells[1].Value.ToString());
+                    dataGridView1.Rows[i].Cells[1].Value = false;
+
+                }
+            }
+        }
+        //@Called by btnSend_Click
+        void turnon(object mac) 
+        {
+            try
+            {
+                WakeFunction(mac.ToString());
+            }
+            catch(Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            
+            }
+        }
+
+      
+    }
+    public class WOLClass : UdpClient
+    {
+        public WOLClass()
+            : base()
+        { }
+        //this is needed to send broadcast packet
+        public void SetClientToBrodcastMode()
+        {
+            if (this.Active)
+                this.Client.SetSocketOption(SocketOptionLevel.Socket,
+                                          SocketOptionName.Broadcast, 0);
+        }
+    }
+}
